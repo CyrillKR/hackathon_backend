5ac35a54a6a99a3ad6c312e3883304ebdaae5cea
jehwaldmann <90551022+jehwaldmann@users.noreply.github.com>
2022-01-12 19:47:15 +0200

    Dockered

--- database/models.js
+++ database/models.js
@@ -63,149 +63,149 @@ const UsersTable = sequelize.define("usersTable", {
 
   slow_songs_or_fast_songs: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: true,
     allowNull: false,
   },
   dance: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   folk: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   country: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   classical_music: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   musical: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   pop: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   rock: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   metal_or_hardrock: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   punk: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   hiphop_rap: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   reggae_ska: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   swing_jazz: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   rock_n_roll: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   alternative: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   latino: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   techno_trance: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   opera: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   //Movies:
 
   horror: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   thriller: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   comedy: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   romantic: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   sci_fi: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   war: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   fantasy_fairy_tales: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   animated: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   documentary: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   western: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   action: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
 
@@ -213,32 +213,32 @@ const UsersTable = sequelize.define("usersTable", {
 
   mathematics: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   physics: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   biology: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   chemistry: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   medicine: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   geography: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
 
@@ -246,27 +246,27 @@ const UsersTable = sequelize.define("usersTable", {
 
   history: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   psychology: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   politics: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   economy: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   law: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
 
@@ -274,17 +274,17 @@ const UsersTable = sequelize.define("usersTable", {
 
   science_and_technology: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   internet: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   pc: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
 
@@ -292,22 +292,22 @@ const UsersTable = sequelize.define("usersTable", {
 
   art_exhibitions: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   theatre: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   dancing: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   musical_instruments: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
 
@@ -315,62 +315,62 @@ const UsersTable = sequelize.define("usersTable", {
 
   writing: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   reading: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   foreign_languages: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   cars: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   religion: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   gardening: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   celebrities: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   shopping: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   fun_with_friends: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   pets: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   sport: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   travel: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
 
@@ -378,69 +378,66 @@ const UsersTable = sequelize.define("usersTable", {
 
   flying: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   thunder_lightning: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   darkness: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   heights: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   spiders: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   snakes: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   rats_mice: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   aging: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   dangerous_dog: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   public_speaking: {
     type: Sequelize.BOOLEAN,
-    default: false,
+    defaultValue: false,
     allowNull: false,
   },
   //Health habits
   smoking: {
     type: Sequelize.STRING,
-    default: false,
-    allowNull: false,
+    defaultValue: "no answer",
   },
   drinking: {
     type: Sequelize.STRING,
-    default: false,
-    allowNull: false,
+    defaultValue: "no answer",
   },
   healthy_life_style: {
     type: Sequelize.BOOLEAN,
-    default: false,
-    allowNull: false,
+    defaultValue: false,
   },
 });
 
---
+++ queries/userQueries.js
@@ -0,0 +1,25 @@
+// const {query} = require("../database/database");
+
+// async function signUpUserQuery(
+//   email,
+//   bio,
+//   password,
+//   firstName,
+//   lastName,
+//   phoneNumber,
+//   verifyPassword
+// ) {
+//   try {
+//     const sql = `INSERT INTO userstables (email, password) VALUES ('${email}', '${password}')`;
+//     const user = await query(sql);
+//     return user;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+
+
+// INSERT INTO userstables (user_id, role, first_name, last_name, picture, gender, birth_year, email, password, phone_number, bio, number_siblings, train, education) VALUES ("abd", "public user","Jessica", "Waldmann", "url", "female", "1991", "email", "password", "83308083", "bio", "3", "train one", "high school");
+
+
