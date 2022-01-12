618f81caf220db47493ee57716eff502950b999f
CyrillKR <88666557+CyrillKR@users.noreply.github.com>
2022-01-12 16:42:24 +0200

    Added image upload to cloudinary, refactored some routes to have a controller template before adding queries

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+const fileUpload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const express = require('express');
 const app = express();
@@ -28,6 +35,7 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
 
 // Routes
 app.use('/api/v1/user', userRoute);
