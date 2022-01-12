f56c599acf88868c50973eb220e97879c44940c1
CyrillKR <88666557+CyrillKR@users.noreply.github.com>
2022-01-12 15:14:30 +0200

    Moved to a new repo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hackathon_backend
+
 The backend server for the ITC 2022 Hackathon
+PORT is 6000
+`npm run start` to start server
+`npm run dev` to start server(nodemon)
---
+++ controllers/usersController.js
@@ -0,0 +1,86 @@
+const userQueries = require("../queries/userQueries");
+const uuidv4 = require('uuid');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require("../errors");
+const { encryptPassword, comparePassword } = require('../utils/bcrypt');
+
+const login = (req, res) => {
+  const token = req.token;
+  res.status(StatusCodes.OK).json({
+    token,
+    userInfo: req.body.user,
+    message: "Login Success!",
+  });
+};
+
+const signUpUser = async (req, res) => {
+  const { email, bio, password, firstName, lastName, phoneNumber, verifyPassword } =
+    req.body;
+
+  if (password !== verifyPassword) {
+    throw new BadRequestError("Passwords don't match");
+  }
+
+  const userId = uuidv4();
+  const hashedPassword = encryptPassword(password);
+  const user = await userQueries.signUpUserQuery(
+    {
+      userId,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      bio
+    }
+  );
+
+  if (!user) {
+    throw new NotFoundError('User not found');
+  }
+
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const updateUserPassword = async (req, res) => {
+  const { newPassword, newVerifyPassword } = req.body;
+
+  if (newPassword !== newVerifyPassword) {
+    throw new BadRequestError("Passwords don't match");
+  }
+
+  const { userId } = req.user;
+  await userQueries.updateUserPasswordQuery(
+    newPassword,
+    newVerifyPassword, userId
+  );
+  res.status(StatusCodes.OK).send("Updated Password Successfully!");
+};
+
+const updateUserProfile = async (req, res) => {
+  const { email, firstName, lastName, phoneNumber, bio } = req.body;
+  await userQueries.updateUserProfileQuery(
+    email,
+    firstName,
+    lastName,
+    phoneNumber,
+    bio
+  );
+  res.status(StatusCodes.OK).send("Updated Profile Successfully!");
+};
+
+const makeAdmin = async (req, res) => {
+  const { id } = req.params;
+  await userQueries.makeAdminQuery(
+    id
+  );
+  res.status(StatusCodes.OK).send("Updated Admin Successfully!");
+};
+
+module.exports = {
+  makeAdmin,
+  updateUserPassword,
+  updateUserProfile,
+  login,
+  signUpUser
+};
